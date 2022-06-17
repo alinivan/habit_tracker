60090e3a03f348d373e8380bc7d4b7f85fd326a2
Alin Ivan <alinivan@192-168-0-187.rdsnet.ro>
2022-06-17 13:29:54 +0300

    added decimal as value for tracker

--- app/Models/Tracker.php
+++ app/Models/Tracker.php
@@ -9,7 +9,7 @@ class Tracker
 {
     public static function insert(array $request): void
     {
-        DB::query("INSERT INTO tracker (habit_id, date, value) VALUES (?,?,?)", [$request['habit_id'], $request['date'], (int)$request['value']]);
+        DB::query("INSERT INTO tracker (habit_id, date, value) VALUES (?,?,?)", [$request['habit_id'], $request['date'], round($request['value'], 1)]);
     }
 
     public static function getToday(): bool|array
