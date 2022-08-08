4894b2d70b35deffffd72f34626dae9b82949181
Alin Ivan <alinivan@192-168-0-187.rdsnet.ro>
2022-08-08 16:34:57 +0300

    fix Enter keycode when modal is closed

--- public/js/main2.js
+++ public/js/main2.js
@@ -2,22 +2,33 @@ function modal_close() {
     $('#modal').html('');
 }
 
-$(document).keypress(function (event) {
-    var keycode = (event.keyCode ? event.keyCode : event.which);
+function modalIsOpen() {
+    if ($('#modal').html().length > 1) {
+        return true;
+    }
+    return false;
+}
 
-    if (keycode === 13) {
-        event.preventDefault();
-        $('.modal-save').trigger('click');
+$(document).keypress(function (event) {
+    if (modalIsOpen()) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+
+        if (keycode === 13) {
+            event.preventDefault();
+            $('.modal-save').trigger('click');
+        }
     }
 });
 
 
 $(document).keyup(function (event) {
-    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if (modalIsOpen()) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
 
-    if (keycode === 27) {
-        event.preventDefault();
-        modal_close();
+        if (keycode === 27) {
+            event.preventDefault();
+            modal_close();
+        }
     }
 });
 
